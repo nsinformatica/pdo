05befd711ddbff9b623f51d86771d81bf87bad49
Neto Sales <contato@netosales.com.br>
2017-11-29 17:50:46 -0300

    Arquivos da aula sobre pdo.

---
+++ exemplo-03.php
@@ -0,0 +1,14 @@
+<?php
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+$stmt = $conn->prepare("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES(:LOGIN, :PASSWORD)");
+$login = "jose";
+$password = "12345678";
+$stmt->bindParam(":LOGIN",$login);
+$stmt->bindParam(":PASSWORD",$password);
+$stmt->execute();
+echo "Inserido OK!";
+
+
+
+
+?>
